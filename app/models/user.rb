df8eaf3738b6c47b7f8dbf86c97ea510f2e9486e
James McCloskey <james@mccloskey.io>
2020-01-17 15:03:17 -0600

    revert admin model, replace with user model
    revert admin model, replace with user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-class Admin < ApplicationRecord
+class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :trackable, :confirmable, :lockable
+         :confirmable, :lockable, :trackable
 end
